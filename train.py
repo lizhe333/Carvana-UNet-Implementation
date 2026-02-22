ecde4507cfd264d3517f485ca0855ea0e711a0fb
lizhe333 <3289870229@qq.com>
2026-02-22 15:25:20 +0800

    complete baseline

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+
 
 from model import Unet
 from dataset import CarvanaDataset
@@ -16,14 +16,14 @@ LEARNING_RATE = 1e-4
 SPLIT=0.2
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 BATCH_SIZE = 4
-EPOCHS = 4
+EPOCHS = 1
 NUM_WORKERS = 4
 IMAGE_HEIGHT = 572
 IMAGE_WIDTH = 572
 PIN_MEMORY = True
 DATAPATH = "../input/carvana-image-masking-challenge/"
-TRAIN_IMG_DIR = './train'
-TRAIN_MASK_DIR = './train_masks'
+TRAIN_IMG_DIR = './dataset/train'
+TRAIN_MASK_DIR = './dataset/train_masks'
 
 import os
 images=os.listdir(TRAIN_IMG_DIR)
@@ -63,13 +63,84 @@ def train_test_split(images,splitSize):
     val_len=int(imageLen*splitSize)
     train_len=imageLen-val_len
     train_images=images[:train_len]
+    #先取少量的进行验证
+    train_images=train_images[:100]
+
     val_images=images[train_len:]
+
+    val_images=val_images[:100]
     return train_images,val_images
 
 train_images,val_images=train_test_split(images,SPLIT)
 
 
-train_dataset=CarvanaDataset(train_images,TRAIN_IMG_DIR,TRAIN_MASK_DIR,train_transform,transform=True)
-val_dataset=CarvanaDataset(val_images,TRAIN_IMG_DIR,TRAIN_MASK_DIR,val_transform,transform=True)
+train_dataset=CarvanaDataset(train_images,TRAIN_IMG_DIR,TRAIN_MASK_DIR,train_transform,train=True)
+val_dataset=CarvanaDataset(val_images,TRAIN_IMG_DIR,TRAIN_MASK_DIR,val_transform,train=False)
 train_loader=DataLoader(train_dataset,batch_size=BATCH_SIZE,shuffle=True)
 val_loader=DataLoader(val_dataset,batch_size=BATCH_SIZE,shuffle=False)
+
+#定义模型，损失函数，优化器
+model=Unet().to(DEVICE)
+train_loss=[]
+val_loss=[] 
+optimizer=optim.Adam(model.parameters(),lr=LEARNING_RATE)
+criterion=nn.BCEWithLogitsLoss()
+
+def train_one_epoch(model,dataloader,optimizer,criterion,device):
+    model.train()
+    running_loss=0.0
+    counter=0
+    for batch in tqdm(dataloader,desc="Training"):
+        images=batch["image"].to(device)
+        masks=batch["mask"].to(device)
+
+        #先清空梯度
+        optimizer.zero_grad()
+        #前向传播
+        pre=model(images) #[B,1,H,W]
+        #计算损失
+        #mask:[B,H,W]
+        #需要先对pre进行一个squeeze
+        pre=pre.squeeze(1) #[B,1,H,W]->[B,H,W]
+        loss=criterion(pre,masks)
+        #反向传播
+        loss.backward()
+        #更新参数
+        optimizer.step()
+        running_loss+=loss.item()
+        counter+=1
+        #计算每个epoch的平均损失
+    epoch_loss=running_loss/counter
+    return epoch_loss
+
+def eval_one_epoch(model,dataloader,criterion,device):
+    model.eval()
+    running_loss=0.0
+    counter=0
+    with torch.no_grad():
+        #不记录梯度，只执行前向传播
+        for batch in tqdm(dataloader,desc="Evaluating"):
+            images=batch["image"].to(device)
+            masks=batch["mask"].to(device)
+
+            #前向传播
+            pre=model(images) #[B,1,H,W]
+            #计算损失
+            #mask:[B,H,W]
+            #需要先对pre进行一个squeeze
+            pre=pre.squeeze(1) #[B,1,H,W]->[B,H,W]
+            
+            loss=criterion(pre,masks)
+            running_loss+=loss.item()
+            counter+=1
+    epoch_loss=running_loss/counter
+    return epoch_loss
+
+train_loss=[]
+val_loss=[]
+if __name__ == "__main__":
+    model=Unet().to(DEVICE)
+    for epoch in range(EPOCHS):
+        train_loss=train_one_epoch(model,train_loader,optimizer,criterion,DEVICE)
+        val_loss=eval_one_epoch(model,val_loader,criterion,DEVICE)
+        print(f"Epoch {epoch+1}/{EPOCHS} Train Loss: {train_loss:.4f} Val Loss: {val_loss:.4f}")
